7744876b1f9e8e1e9cf4320f96b944b8edb4c969
yll <simyululi@163.com>
2021-06-05 23:33:02 +0800

    更新轮播图

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,17 +1,129 @@
 <template>
-  <div>
+  <div class="index">
+    <div class="container">
+      <div class="swiper-box">
+        <swiper :options="swiperOption">
+          <swiper-slide v-for="(item,index) in slideList" :key="index">
+            <a :href="'/#/product/'+item.id" target="_blank"><img :src="item.img"></a>
+          </swiper-slide>
+          <!-- Optional controls -->
+          <div class="swiper-pagination"  slot="pagination"></div>
+          <div class="swiper-button-prev" slot="button-prev"></div>
+          <div class="swiper-button-next" slot="button-next"></div>
+        </swiper>
+      </div>
+    </div>
     <service-bar></service-bar>
   </div>
 </template>
 
 <script>
+import {Swiper, SwiperSlide} from "vue-awesome-swiper"
 import ServiceBar from "@/components/ServiceBar";
+import 'swiper/css/swiper.css'
 export default {
   name: "index",
-  components: {ServiceBar}
+  components: {ServiceBar, Swiper, SwiperSlide},
+  data(){
+    return{
+      swiperOption:{
+        autoplay:true,
+        loop:true,
+        effect:'cube',
+        cubeEffect: {
+          shadowOffset: 100,
+          shadowScale: 0.6
+        },
+        pagination: {
+          el: '.swiper-pagination',
+          clickable:true
+        },
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        }
+      },
+      slideList:[
+        {
+          id:'42',
+          img:'/imgs/slider/slide-1.jpg'
+        },
+        {
+          id:'45',
+          img:'/imgs/slider/slide-2.jpg'
+        },
+        {
+          id:'46',
+          img:'/imgs/slider/slide-3.jpg'
+        },
+        {
+          id:'',
+          img:'/imgs/slider/slide-4.jpg'
+        },
+        {
+          id:'',
+          img:'/imgs/slider/slide-1.jpg'
+        }
+      ],
+      menuList:[
+        [
+          {
+            id:30,
+            img:'/imgs/item-box-1.png',
+            name:'小米CC9',
+          },{
+          id:31,
+          img:'/imgs/item-box-2.png',
+          name:'小米8青春版',
+        },{
+          id:32,
+          img:'/imgs/item-box-3.jpg',
+          name:'Redmi K20 Pro',
+        },{
+          id:33,
+          img:'/imgs/item-box-4.jpg',
+          name:'移动4G专区',
+        }
+        ],
+        [0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]
+      ],
+      adsList:[
+        {
+          id:33,
+          img:'/imgs/ads/ads-1.png'
+        },{
+          id:48,
+          img:'/imgs/ads/ads-2.jpg'
+        },{
+          id:45,
+          img:'/imgs/ads/ads-3.png'
+        },{
+          id:47,
+          img:'/imgs/ads/ads-4.jpg'
+        }
+      ],
+      phoneList:[],
+      showModal:false
+    }
+  }
 }
 </script>
 
-<style scoped>
-
+<style lang="scss">
+  @import './../assets/scss/config.scss';
+  @import './../assets/scss/mixin.scss';
+  .index{
+    .swiper-box{
+      .swiper-container {
+        height: 451px;
+        .swiper-button-prev{
+          left:274px;
+        }
+        img{
+          width:100%;
+          height:100%;
+        }
+      }
+    }
+  }
 </style>
